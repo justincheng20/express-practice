bf54f04acc739d2b7692fe72f6d90f663eb6c665
justincheng20 <justincheng20@gmail.com>
2020-04-28 17:38:48 -0700

    Add error

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/mean", function(req, res) {
+app.get("/mean", function (req, res) {
   let nums = req.query.nums.split(",");
-  let sum = 0;
-  nums.forEach(num => sum += +num);
-  console.log(sum);
-  return res.send(res.json({operation: "mean", value: sum/nums.length}));
+  try {
+  return res.send(res.json({ operation: "mean", value: mean(nums) }));
+  } catch {
+    return res.send(res.json({operation: "mean", value: "invalid" }))
+  }
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App on port 3000");
 });
+
+let mean = nums => {
+  let sum = 0;
+  nums.forEach(num => {
+    if (!isNaN(+num)) {
+      console.log("ok")
+      sum += +num;
+    } else {
+      throw(`${num} is not a number`);
+    }
+  });
+  return sum / nums.length;
+};
+
+let median = nums => {
+
+}
